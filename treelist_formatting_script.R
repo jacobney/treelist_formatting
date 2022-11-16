6b99f67147b68279805f73f3c11466a9232f327b
Jacob Ney <jacobney@colostate.edu>
2022-11-15 18:05:16 -0700

    treelist_formatting files
    This is most of the code right now to convert tree lists from JZ to the .csv's needed for input to QuicFire

---
+++ treelist_formatting_script.R
@@ -0,0 +1,35 @@
+#This script will help convert the treelists into the format expected by the QUIC-Fire Script
+
+library(readxl)
+library(tidyverse)
+
+#This is the script to see changes for one csv at a time
+
+OG_data <- read.csv("C:\\Users\\neyja\\OneDrive\\Documents\\GitHub\\treelist_formatting\\conversions\\prerun1treelist.csv")
+crown <- filter(OG_data, output == ".TRUE.")
+crown_measures <- mutate(crown, cr = cw / 2, dbh = 0)
+crown_vars <- subset(crown_measures, select = -c(output, id, cw))
+csv_ready <- unite(crown_vars, x, x, y, dbh, ht, cbh, cr,  sep = " ")
+write.table(csv_ready, paste("C:\\Users\\neyja\\OneDrive\\Documents\\GitHub\\treelist_formatting\\conversions\\R_Out_prerun1treelist.csv"), 
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+#This is the batch process to run for an entire directory
+#NEEDS UPDATING
+
+setwd("C:\\Users\\16516\\Desktop\\R_Projects\\treelist_formatting\\treelists\\")
+raw_files <- Sys.glob( "*.csv" )
+for( i in raw_files )
+{
+  OG_data <- read.csv( i )
+  crown <- filter(OG_data, output == ".TRUE.")
+  crown_measures <- mutate(crown, cr = cw / 2, dbh = 0)
+  crown_vars <- subset(crown_measures, select = -c(output, id, cw))
+  csv_ready <- unite(crown_vars, x, x, y, dbh, ht, cbh, cr,  sep = " ")
+  write.table(csv_ready, paste( "C:\\Users\\16516\\Desktop\\\\R_Projects\\treelist_formatting\\treelists\\Output_", i, sep = " "), 
+        row.names = FALSE, col.names = FALSE, quote = FALSE)
+}
+
+#Useful Filepaths:
+#General Working Directory: "C:\\Users\\16516\\Desktop\\R_Projects\\treelist_formatting\\
+#Prerun directory: "C:\\Users\\16516\\Desktop\\\\R_Projects\\treelist_formatting\\prerun_treelists\\
+#Run directory: "C:\\Users\\16516\\Desktop\\\\R_Projects\\treelist_formatting\\treelists\\
