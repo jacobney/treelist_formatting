fa5a28b124e2fcb46db2e80be7491069e2f08d3f
Jacob Ney <jacobney@colostate.edu>
2022-12-04 13:06:32 -0700

    New Filepaths

--- treelist_formatting_script.R
+++ treelist_formatting_script.R
@@ -7,12 +7,12 @@ library(tidyverse)
   
   #prepares JZ treelist to appropriate format
 
-OG_data <- read.csv("C:\\Users\\neyja\\OneDrive\\Documents\\GitHub\\treelist_formatting\\prerun1treelist.csv")
+OG_data <- read.csv("C:\\Users\\neyja\\OneDrive - Colostate\\Documents\\GitHub\\treelist_formatting\\prerun1treelist.csv")
 crown <- filter(OG_data, output == ".TRUE.")
 crown_measures <- mutate(crown, cr = cw / 2, dbh = 0)
 crown_vars <- subset(crown_measures, select = -c(output, id, cw))
 csv_ready <- unite(crown_vars, x, x, y, dbh, ht, cbh, cr,  sep = " ")
-write.table(csv_ready, paste("C:\\Users\\neyja\\OneDrive\\Documents\\GitHub\\treelist_formatting\\treelist.csv"), 
+write.table(csv_ready, paste("C:\\Users\\neyja\\OneDrive - Colostate\\Documents\\GitHub\\treelist_formatting\\output_prerun1treelist.csv"),
             row.names = FALSE, col.names = FALSE, quote = FALSE)
 
   #Pulls, max, min, and mean of treelist and writes table
@@ -27,12 +27,12 @@ treelist_upper <- max %>%
   full_join(mean, by = c("x", "y", "dbh", "ht", "cbh", "cr"))
 treelist_sum <- treelist_upper %>%
   full_join(min, by = c("x", "y", "dbh", "ht", "cbh", "cr"))
-write.table(treelist_sum, paste("C:\\Users\\neyja\\OneDrive\\Documents\\GitHub\\treelist_formatting\\prerun1treelist_sum.csv"), 
+write.table(treelist_sum, paste("C:\\Users\\neyja\\OneDrive - Colostate\\Documents\\GitHub\\treelist_formatting\\summary_prerun1treelist.csv"), 
             row.names = FALSE, col.names = TRUE, quote = FALSE)
 
 #This is the batch process to run for an entire directory
 
-setwd("C:\\Users\\neyja\\OneDrive\\Documents\\Bitvise_SSH\\Temp4Servers\\OG_treelist_csvs\\prerun_treelists")
+setwd("C:\\Users\\neyja\\data_desktop\\OG_treelist_csvs\\preruns")
 raw_files <- Sys.glob( "*.csv" )
 for( i in raw_files )
 {
@@ -41,9 +41,9 @@ for( i in raw_files )
   crown_measures <- mutate(crown, cr = cw / 2, dbh = 0)
   crown_vars <- subset(crown_measures, select = -c(output, id, cw))
   csv_ready <- unite(crown_vars, x, x, y, dbh, ht, cbh, cr,  sep = " ")
-  write.table(csv_ready, paste( "C:\\Users\\neyja\\OneDrive\\Documents\\Bitvise_SSH\\Temp4Servers\\formatted_treelists\\prerun\\Outputs\\Output_", i, sep = ""), 
+  write.table(csv_ready, paste( "C:\\Users\\neyja\\data_desktop\\fomatted_treelists\\preruns\\outputs\\output_", i, sep = ""), 
         row.names = FALSE, col.names = FALSE, quote = FALSE)
-  
+ 
   find_max <- sapply(crown_vars, max)
   max <- data.frame(t(find_max))
   find_min <- sapply(crown_vars, min)
@@ -54,6 +54,6 @@ for( i in raw_files )
     full_join(mean, by = c("x", "y", "dbh", "ht", "cbh", "cr"))
   treelist_sum <- treelist_upper %>%
     full_join(min, by = c("x", "y", "dbh", "ht", "cbh", "cr"))
-  write.table(treelist_sum, paste("C:\\Users\\neyja\\OneDrive\\Documents\\Bitvise_SSH\\Temp4Servers\\formatted_treelists\\prerun\\Summaries\\Summary_", i, sep = ""), 
+  write.table(treelist_sum, paste("C:\\Users\\neyja\\data_desktop\\fomatted_treelists\\preruns\\summaries\\summary_", i, sep = " "), 
               row.names = FALSE, col.names = TRUE, quote = FALSE)
 }
