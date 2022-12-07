740f5898d656a0246df07c7e1fd74b8ddbe1fcc1
Jacob Ney <jacobney@colostate.edu>
2022-12-07 13:58:21 -0700

    Shrunk domain, added code sections

--- treelist_formatting_script.R
+++ treelist_formatting_script.R
@@ -5,15 +5,14 @@ library(writexl)
 library(tidyverse)
 library(dplyr)
 
-#This is the script to see changes for one csv at a time
-  
+###This is the script to see changes for one csv at a time################################################################################
   #prepares JZ treelist to appropriate format
 
 OG_data <- read.csv("C:\\Users\\neyja\\OneDrive - Colostate\\Documents\\GitHub\\treelist_formatting\\prerun1treelist.csv")
 crown <- filter(OG_data, output == ".TRUE.")
-crown_measures <- mutate(crown, cr = cw / 2, dbh = 0)
-crown_vars <- subset(crown_measures, select = -c(output, id, cw))
-csv_ready <- unite(crown_vars, x, x, y, dbh, ht, cbh, cr,  sep = " ")
+crown_measures <- mutate(crown, cr = cw / 2, dbh = 0, x_new = x - 500)
+crown_vars <- subset(crown_measures, select = c(x_new, y, dbh, ht, cbh, cr))
+csv_ready <- unite(crown_vars, x.y.dbh.ht.cbh.cr, x_new, y, dbh, ht, cbh, cr,  sep = " ")
 write.table(csv_ready, paste("C:\\Users\\neyja\\OneDrive - Colostate\\Documents\\GitHub\\treelist_formatting\\output_prerun1treelist.csv"),
             row.names = FALSE, col.names = FALSE, quote = FALSE)
 
@@ -26,14 +25,12 @@ min <- data.frame(t(find_min))
 find_mean <- sapply(crown_vars, mean)
 mean <- data.frame(t(find_mean))
 treelist_upper <- max %>%
-  full_join(mean, by = c("x", "y", "dbh", "ht", "cbh", "cr"))
+  full_join(mean, by = c("x_new", "y", "dbh", "ht", "cbh", "cr"))
 treelist_sum <- treelist_upper %>%
-  full_join(min, by = c("x", "y", "dbh", "ht", "cbh", "cr"))
-#metric <- c("Max", "Mean", "Min")
-#summary_treelist <- cbind(treelist_sum, metric)
+  full_join(min, by = c("x_new", "y", "dbh", "ht", "cbh", "cr"))
 write_csv(treelist_sum, paste("C:\\Users\\neyja\\data_desktop\\summary_prerun1treelist.csv"))
 
-#This is the batch process to run for an entire directory
+###This is the batch process to run for an entire directory##################################################################################
 
 setwd("C:\\Users\\neyja\\data_desktop\\OG_treelist_csvs\\preruns")
 raw_files <- Sys.glob( "*.csv" )
@@ -41,9 +38,9 @@ for( i in raw_files )
 {
   OG_data <- read.csv( i )
   crown <- filter(OG_data, output == ".TRUE.")
-  crown_measures <- mutate(crown, cr = cw / 2, dbh = 0)
-  crown_vars <- subset(crown_measures, select = -c(output, id, cw))
-  csv_ready <- unite(crown_vars, x, x, y, dbh, ht, cbh, cr,  sep = " ")
+  crown_measures <- mutate(crown, cr = cw / 2, dbh = 0, x_new = x - 500)
+  crown_vars <- subset(crown_measures, select = c(x_new, y, dbh, ht, cbh, cr))
+  csv_ready <- unite(crown_vars, x.y.dbh.ht.cbh.cr, x_new, y, dbh, ht, cbh, cr,  sep = " ")
   write.table(csv_ready, paste( "C:\\Users\\neyja\\data_desktop\\fomatted_treelists\\preruns\\outputs\\output_", i, sep = ""), 
         row.names = FALSE, col.names = FALSE, quote = FALSE)
  
@@ -54,15 +51,13 @@ for( i in raw_files )
   find_mean <- sapply(crown_vars, mean)
   mean <- data.frame(t(find_mean))
   treelist_upper <- max %>%
-    full_join(mean, by = c("x", "y", "dbh", "ht", "cbh", "cr"))
+    full_join(mean, by = c("x_new", "y", "dbh", "ht", "cbh", "cr"))
   treelist_sum <- treelist_upper %>%
-    full_join(min, by = c("x", "y", "dbh", "ht", "cbh", "cr"))
-#  metric <- c("Max", "Mean", "Min")
-#  summary_treelist <- cbind(treelist_sum, metric)
+    full_join(min, by = c("x_new", "y", "dbh", "ht", "cbh", "cr"))
   write_csv(treelist_sum, paste("C:\\Users\\neyja\\data_desktop\\fomatted_treelists\\preruns\\summaries\\summary_", i, sep = " "))
 }
 
-#This is the batch process to summarize summaries for overall domain specs
+###This is the batch process to summarize summaries for overall domain specs########################################################
 
 library(data.table)
 setwd("C:\\Users\\neyja\\data_desktop\\fomatted_treelists\\preruns\\summaries")
@@ -78,9 +73,9 @@ min <- data.frame(t(find_min))
 find_mean <- sapply(summary_combined, mean)
 mean <- data.frame(t(find_mean))
 treelist_upper <- max %>%
-  full_join(mean, by = c("x", "y", "dbh", "ht", "cbh", "cr"))
+  full_join(mean, by = c("x_new", "y", "dbh", "ht", "cbh", "cr"))
 treelist_sum <- treelist_upper %>%
-  full_join(min, by = c("x", "y", "dbh", "ht", "cbh", "cr"))
+  full_join(min, by = c("x_new", "y", "dbh", "ht", "cbh", "cr"))
 metric <- c("Max", "Mean", "Min")
 grand_summary <- cbind(treelist_sum, metric)
 write_csv(grand_summary, paste("C:\\Users\\neyja\\data_desktop\\grand_summary.csv"))
